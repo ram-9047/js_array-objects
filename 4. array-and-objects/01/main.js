616f0be403823217a54aba793bb5649929edcb82
Ram S Gupta <ramg786@live.com>
2019-09-12 22:41:56 +0530

    objects done except last to questions and array&objects completed

---
+++ 4. array-and-objects/01/main.js
@@ -0,0 +1,9 @@
+let favoriteFoods = ["apple","banana","mango"]
+console.log(favoriteFoods[1])
+var firstFavoriteFood = favoriteFoods[0]
+firstFavoriteFood[3] = "pears"
+console.log(favoriteFoods.length)
+console.log(favoriteFoods.splice((favoriteFoods.length - 1), 1 , "pineapple"))
+var lastFavoriteFood = console.log(favoriteFoods[favoriteFoods.length - 1])
+for(var i = 0; i< favoriteFoods.length; i++){
+console.log(favoriteFoods[i])}
