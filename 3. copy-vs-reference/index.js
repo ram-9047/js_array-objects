b233471399b13fa58ba7ae0af54e8fa90cc38980
Ram S Gupta <ramg786@live.com>
2019-09-13 16:12:41 +0530

    copy-vs-refdone

--- 3. copy-vs-reference/index.js
+++ 3. copy-vs-reference/index.js
@@ -1,40 +1,40 @@
 // Value of x, y, a, b
 
-var x = 10;
-var y = "abc";
-var a = x;
-var b = y;
+var x = 10; //10
+var y = "abc"; //"abc"
+var a = x; // 10
+var b = y; //"abc"
 
 // Value of x, y, a, b
-var x = 10;
-var y = "abc";
-var a = x;
-var b = y;
-a = 5;
-b = "def";
+var x = 10; // 10
+var y = "abc"; //"abc"
+var a = x;// 10
+var b = y;// "abc"
+a = 5;// 5
+b = "def"; //"def"
 
 // Value of arr & arrCopy
 var arr = [1];
-var arrCopy = arr;
-arr.push(2);
+var arrCopy = arr; // 1
+arr.push(2); // 2
 
 // Value of obj
-var obj = { name: "ryan" };
-obj = { surname: "florance" };
+var obj = { name: "ryan" }; //"ryan"
+obj = { surname: "florance" }; //"florance"
 
 // What's the output.
-var arr = ["Hi"];
-var arr2 = arr;
-console.log(arr === arr2);
+var arr = ["Hi"]; // Hi
+var arr2 = arr; // Hi
+console.log(arr === arr2); // true
 
 // What's the output.
-var arr1 = ["Hi!"];
-var arr2 = ["Hi!"];
-console.log(arr1 === arr2);
-console.log(arr == arr2);
+var arr1 = ["Hi!"]; //Hi
+var arr2 = ["Hi!"]; // Hi
+console.log(arr1 === arr2); // false
+console.log(arr == arr2); //false
 
 //What's the output
-console.log([10] === [10]);
+console.log([10] === [10]); //true
 
 // What's the output?
 
@@ -56,33 +56,33 @@ console.log(personObj1); // -> ?
 console.log(personObj2); // -> ?
 
 // Guess the output
-var oldArray = [];
-var object = {};
-object.newArray = oldArray;
-oldArray.push(10);
-console.log(object.newArray === oldArray);
+var oldArray = []; //empty array []
+var object = {}; // empty object {}
+object.newArray = oldArray; //empty array []
+oldArray.push(10); // oldArray = [10]
+console.log(object.newArray === oldArray); // true
 
 // Guess the output
-var a = 5;
-var b = a;
-a = 10;
-console.log(a);
-console.log(b);
+var a = 5; // 5
+var b = a; // 5
+a = 10; // 10 replaces 5
+console.log(a); //10
+console.log(b); //5
 
 // What's the output?
-var a = {};
-var b = a;
-a.a = 1;
-console.log(a);
-console.log(b);
+var a = {}; //empty object
+var b = a; //empty object assign in "b"
+a.a = 1; // "a" key with value "1" is stored in object "a"
+console.log(a); // a is 1
+console.log(b); // a is 1
 
 // What's the output.
-var a = [];
-var b = a;
-a.push(1);
+var a = []; //empty array
+var b = a; //empty array is assigned to "b"
+a.push(1); //now array "a" got one value "1" having index "0"
 console.log(a); // [1]
 console.log(b); // [1]
-console.log(a === b);
+console.log(a === b); // true
 
 // Clone the object person in clone
 var person = {
@@ -95,3 +95,6 @@ var person = {
 };
 
 var clone = {};
+for (let key in person) {
+  clone[key] = person[key];
+}
